b4f294414ce269d8cb75bf6ea23e0d7e58d4117c
YanHaoChen <coolking29@gmail.com>
2019-05-08 13:31:41 +0800

    Initial Commit

---
+++ AQI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AQI
+//
+//  Created by sean on 2019/5/8.
+//  Copyright © 2019 sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
